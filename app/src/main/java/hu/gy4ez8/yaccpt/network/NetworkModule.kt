45e2c5f06a8e4a55423b80f34e7a567300e6833c
Nyakas Péter <thenyakaspeter@gmail.com>
2021-04-18 13:44:06 +0200

    Generated classes with Swagger

--- app/src/main/java/hu/gy4ez8/yaccpt/network/NetworkModule.kt
+++ app/src/main/java/hu/gy4ez8/yaccpt/network/NetworkModule.kt
@@ -1,24 +1,51 @@
 package hu.gy4ez8.yaccpt.network
 
+import com.squareup.moshi.FromJson
+import com.squareup.moshi.Moshi
+import com.squareup.moshi.ToJson
 import dagger.Module
 import dagger.Provides
 import okhttp3.OkHttpClient
 import retrofit2.Retrofit
+import retrofit2.converter.moshi.MoshiConverterFactory
+import java.math.BigDecimal
+import java.util.concurrent.TimeUnit
 import javax.inject.Singleton
+
+object BigDecimalAdapter {
+    @FromJson
+    fun fromJson(string: String) = BigDecimal(string)
+
+    @ToJson
+    fun toJson(value: BigDecimal) = value.toString()
+}
 
 @Module
 class NetworkModule {
     @Provides
     @Singleton
-    fun provideOkHttpClient(): OkHttpClient {
-        return OkHttpClient.Builder().build()
+    fun providesMoshi(): Moshi {
+        return Moshi.Builder()
+            .add(BigDecimalAdapter)
+            .build()
     }
 
     @Provides
     @Singleton
-    fun provideCoinsApi(client: OkHttpClient): CoinsApi {
+    fun provideOkHttpClient(): OkHttpClient {
+        return OkHttpClient.Builder()
+            .connectTimeout(10, TimeUnit.SECONDS)
+            .readTimeout(10, TimeUnit.SECONDS)
+            .writeTimeout(10, TimeUnit.SECONDS)
+            .build()
+    }
+
+    @Provides
+    @Singleton
+    fun provideCoinsApi(client: OkHttpClient, moshi: Moshi): CoinsApi {
         val retrofit = Retrofit.Builder()
             .client(client)
+            .addConverterFactory(MoshiConverterFactory.create(moshi))
             .baseUrl(NetworkConfig.COINLORE_API_ENDPOINT_ADDRESS)
             .build()
         return retrofit.create(CoinsApi::class.java)
